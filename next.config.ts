a1326500c1402387c957c0c0323080f91ddfdc90
Shiqi <450008475@qq.com>
2025-03-12 21:00:22 -0500

    init

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath:"portfolio",
+  basePath: "/portfolio",
   output: "export",
   reactStrictMode: true
 };
