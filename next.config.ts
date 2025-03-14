396c0bbdb1b791081ab98e45e64a17a71d0d369b
Shiqi <450008475@qq.com>
2025-03-14 15:56:00 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.BASE_PATH,
-  output: process.env.OUTPUT,
+  output: process.env.NODE_ENV === "production" ? "export" : "standalone",
   distDir: 'out',
   images: {
     remotePatterns: [
